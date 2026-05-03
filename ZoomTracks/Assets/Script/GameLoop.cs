f707deb387cf7fcf6d060456cd22a82526b0836c
Kevin Vu <kevin.m.vu@gmail.com>
2026-05-03 00:52:48 -0700

    Test ALINE drawing package

--- ZoomTracks/Assets/Script/GameLoop.cs
+++ ZoomTracks/Assets/Script/GameLoop.cs
@@ -1,7 +1,9 @@
+using Drawing;
 using System;
 using UnityEngine;
 
 public class GameLoop : MonoBehaviour {
+
     void Awake() {
         Debug.Log($"GameLoop Awake on object='{this.gameObject.name}' in scene='{this.gameObject.scene.name}'");
         QualitySettings.maxQueuedFrames = 0;
@@ -16,5 +18,18 @@ public class GameLoop : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         Debug.Log(DateTime.Now.Ticks);
+        this.Update_TestAline();
+    }
+
+    private Matrix4x4 originMatrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, Vector3.one);
+
+    private void Update_TestAline() {
+        Draw.ingame.Line(Vector3.zero, Vector3.one * 50, Color.red);
+        using (Draw.ingame.WithMatrix(this.originMatrix)) {
+            using (Draw.ingame.WithColor(Color.blue)) {
+                Draw.ingame.WireBox(Vector3.zero, Vector3.one * 100);
+                Draw.ingame.WireCylinder(Vector3.up * 1f, Vector3.up, 1f, 0.5f);
+            }
+        }
     }
 }
