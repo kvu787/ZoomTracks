d0aa3ee3db030ca0b227ca5bc9d19531130b2d6d
Kevin Vu <kevin.m.vu@gmail.com>
2026-05-06 00:28:50 -0700

    Refactor QuitOnException

--- ZoomTracks/Assets/Scripts/QuitOnException.cs
+++ ZoomTracks/Assets/Scripts/QuitOnException.cs
@@ -6,29 +6,32 @@ namespace ZoomTracks {
         // This attribute ensures the method runs automatically at startup
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
         private static void Init() {
+            RemoveHandlers();
+
             // Catch Unity-logged exceptions (Main thread and Unity-managed worker threads)
             // NOTE: This will catch calls to "Debug.LogException"
-            Application.logMessageReceivedThreaded -= HandleLog;
             Application.logMessageReceivedThreaded += HandleLog;
 
             // Catch raw .NET unhandled exceptions (Unmanaged background threads)
-            AppDomain.CurrentDomain.UnhandledException -= HandleUnhandledException;
             AppDomain.CurrentDomain.UnhandledException += HandleUnhandledException;
         }
 
         private static void HandleLog(string logString, string stackTrace, LogType logType) {
             if (logType == LogType.Exception) {
-                Application.logMessageReceivedThreaded -= HandleLog;
-                AppDomain.CurrentDomain.UnhandledException -= HandleUnhandledException;
+                RemoveHandlers();
                 TriggerExit();
             }
         }
 
         private static void HandleUnhandledException(object sender, UnhandledExceptionEventArgs e) {
-            Application.logMessageReceivedThreaded -= HandleLog;
-            AppDomain.CurrentDomain.UnhandledException -= HandleUnhandledException;
+            RemoveHandlers();
             Debug.LogException((Exception)e.ExceptionObject);
             TriggerExit();
+        }
+
+        private static void RemoveHandlers() {
+            Application.logMessageReceivedThreaded -= HandleLog;
+            AppDomain.CurrentDomain.UnhandledException -= HandleUnhandledException;
         }
 
         private static void TriggerExit() {
