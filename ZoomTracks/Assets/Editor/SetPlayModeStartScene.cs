a1ff632beadc977646d4331b533bdba96ad61434
Kevin Vu <kevin.m.vu@gmail.com>
2026-05-08 09:26:03 -0700

    Add editor script that ensures editor play mode always runs only MainScene, regardless of what scenes are currently in the Hierarchy

---
+++ ZoomTracks/Assets/Editor/SetPlayModeStartScene.cs
@@ -0,0 +1,23 @@
+// Assets/Editor/SetPlayModeScene.cs
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace ZoomTracks {
+    public class SetPlayModeStartScene : AssetPostprocessor {
+        private const string MainScenePath = "Assets/Scenes/MainScene.unity";
+
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths, bool didDomainReload) {
+            if (didDomainReload) {
+                SceneAsset scene = AssetDatabase.LoadAssetAtPath<SceneAsset>(MainScenePath);
+
+                if (scene == null) {
+                    EditorSceneManager.playModeStartScene = null;
+                    UnityEngine.Debug.LogWarning($"SetPlayModeStartScene: Main scene not found at: {MainScenePath}");
+                    return;
+                }
+
+                EditorSceneManager.playModeStartScene = scene;
+            }
+        }
+    }
+}
