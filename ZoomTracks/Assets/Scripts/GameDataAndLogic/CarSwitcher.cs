0cb54cc6f7105629aea737b6f0c35674b05b0437
Kevin Vu <kevin.m.vu@gmail.com>
2026-05-11 00:43:52 -0700

    Fix: Prev/next swap, CarSwitcher must reset transform when switching cars

--- ZoomTracks/Assets/Scripts/GameDataAndLogic/CarSwitcher.cs
+++ ZoomTracks/Assets/Scripts/GameDataAndLogic/CarSwitcher.cs
@@ -11,12 +11,14 @@ namespace ZoomTracks {
 
         private readonly List<Car> Cars;
         private const string GarageFileName = "Garage.json";
+        private readonly TrackObjects TrackObjects;
 
         public CarSwitcher(TrackObjects trackObjects, TrackSwitcher trackSwitcher) {
+            this.TrackObjects = trackObjects;
             string filePath = Path.Combine(Application.streamingAssetsPath.Replace('/', '\\'), GarageFileName);
             Assert.IsTrue(File.Exists(filePath), $"Garage JSON file does not exist at {filePath}");
             string fileContents = File.ReadAllText(filePath); // TODO: Use async file read
-            Garage garage = new(fileContents, trackObjects.PlaceholderCar.transform, trackSwitcher.CurrentTrackScene);
+            Garage garage = new(fileContents, this.TrackObjects.PlaceholderCar.transform, trackSwitcher.CurrentTrackScene);
             this.CurrentCarIndex = garage.StartCarIndex;
             this.Cars = garage.Cars;
             trackObjects.PlaceholderCar.SetActive(false);
@@ -42,12 +44,13 @@ namespace ZoomTracks {
             this.CurrentCar.GameObject.SetActive(false);
             int i = this.CurrentCarIndex;
             if (isNextCar) {
-                i = (i - 1 + this.Cars.Count) % this.Cars.Count;
-            } else if (isPrevCar) {
                 i = (i + 1) % this.Cars.Count;
+            } else if (isPrevCar) {
+                i = (i - 1 + this.Cars.Count) % this.Cars.Count;
             }
             this.CurrentCarIndex = i;
             this.CurrentCar.GameObject.SetActive(true);
+            this.CurrentCar.GameObject.transform.SetFrom(this.TrackObjects.PlaceholderCar.transform);
             return true;
         }
     }
