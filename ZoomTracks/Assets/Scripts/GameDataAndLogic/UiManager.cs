d1f11308fbd2bc7913f5cedee5628bf71f41804e
Kevin Vu <kevin.m.vu@gmail.com>
2026-05-14 05:49:31 -0700

    Make UI and code consistent

--- ZoomTracks/Assets/Scripts/GameDataAndLogic/UiManager.cs
+++ ZoomTracks/Assets/Scripts/GameDataAndLogic/UiManager.cs
@@ -6,7 +6,7 @@ namespace ZoomTracks {
         private CameraPivotManager CameraPivotManager { get; }
         private ControlModeSwitcher ControlModeSwitcher { get; }
         private CarControlModeSwitcher CarControlModeSwitcher { get; }
-        private TMP_Text CameraFollowsCarLabel { get; }
+        private TMP_Text CameraFollowsCarLocationLabel { get; }
         private TMP_Text ControlModeLabel { get; }
         private TMP_Text CarControlModeLabel { get; }
 
@@ -14,13 +14,13 @@ namespace ZoomTracks {
             this.CameraPivotManager = cameraFocuser;
             this.ControlModeSwitcher = controlModeSwitcher;
             this.CarControlModeSwitcher = carControlModeSwitcher;
-            this.CameraFollowsCarLabel = GameObject.Find(nameof(this.CameraFollowsCarLabel)).GetComponent<TMP_Text>();
+            this.CameraFollowsCarLocationLabel = GameObject.Find(nameof(this.CameraFollowsCarLocationLabel)).GetComponent<TMP_Text>();
             this.ControlModeLabel = GameObject.Find(nameof(this.ControlModeLabel)).GetComponent<TMP_Text>();
             this.CarControlModeLabel = GameObject.Find(nameof(this.CarControlModeLabel)).GetComponent<TMP_Text>();
         }
 
         public void UpdateUi() {
-            this.CameraFollowsCarLabel.text = $"Camera follows car location: {this.CameraPivotManager.FollowsCarLocation}";
+            this.CameraFollowsCarLocationLabel.text = $"Camera follows car location: {this.CameraPivotManager.FollowsCarLocation}";
             this.ControlModeLabel.text = $"Control mode: {this.ControlModeSwitcher.Mode}";
             this.CarControlModeLabel.text = $"Car control mode: {this.CarControlModeSwitcher.Mode}";
         }
