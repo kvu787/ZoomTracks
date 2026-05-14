23e175ed6315938810d8abc81bb9c524407b1733
Kevin Vu <kevin.m.vu@gmail.com>
2026-05-14 05:42:47 -0700

    Set default for "camera follows car" to true, reorder code

--- ZoomTracks/Assets/Scripts/GameDataAndLogic/CameraFocuser.cs
+++ ZoomTracks/Assets/Scripts/GameDataAndLogic/CameraFocuser.cs
@@ -3,24 +3,25 @@ using UnityEngine.Assertions;
 
 namespace ZoomTracks {
     public class CameraFocuser {
+        private CarState CarState { get; }
         private InputManager InputManager { get; }
         private Transform CameraPanAndYaw { get; }
-        private CarState CarState { get; }
-        public bool FollowsCar { get; private set; }
         private TransformStruct OriginalCameraPanAndYawTransform { get; }
 
         public CameraFocuser(CarState carState, InputManager inputManager) {
-            this.InputManager = inputManager;
             this.CarState = carState;
+            this.InputManager = inputManager;
             this.CameraPanAndYaw = GameObject.Find(nameof(this.CameraPanAndYaw)).transform;
-
             Assert.IsTrue(this.CameraPanAndYaw.localPosition.y == 0);
             Assert.IsTrue(this.CameraPanAndYaw.localEulerAngles.x == 0);
             Assert.IsTrue(this.CameraPanAndYaw.localEulerAngles.z == 0);
             Assert.IsTrue(this.CameraPanAndYaw.localScale == Vector3.one);
-
             this.OriginalCameraPanAndYawTransform = new TransformStruct(this.CameraPanAndYaw.transform);
+
+            this.FollowsCar = true;
         }
+
+        public bool FollowsCar { get; private set; }
 
         public void ReadInputAndToggleFocus() {
             // A key: Toggle follow
