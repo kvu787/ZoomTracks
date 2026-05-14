665f90bfb55e2fde276a505a15b853fa86afc2ea
Kevin Vu <kevin.m.vu@gmail.com>
2026-05-14 06:09:23 -0700

    Refactor SetPlayModeStartScene

--- ZoomTracks/Assets/Editor/SetPlayModeStartScene.cs
+++ ZoomTracks/Assets/Editor/SetPlayModeStartScene.cs
@@ -1,21 +1,19 @@
-// Assets/Editor/SetPlayModeScene.cs
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using UnityEngine;
 
 namespace ZoomTracks {
-    public class SetPlayModeStartScene : AssetPostprocessor {
-        private const string MainScenePath = "Assets/Scenes/Main.unity";
+    // https://docs.unity3d.com/6000.3/Documentation/ScriptReference/AssetPostprocessor.OnPostprocessAllAssets.html
+    // https://docs.unity3d.com/6000.3/Documentation/ScriptReference/SceneManagement.EditorSceneManager-playModeStartScene.html
+    class SetPlayModeStartScene : AssetPostprocessor {
+        private const string MainScenePath = "Assets/Scenes/ain.unity";
 
-        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths, bool didDomainReload) {
-            if (didDomainReload) {
-                SceneAsset scene = AssetDatabase.LoadAssetAtPath<SceneAsset>(MainScenePath);
-
-                if (scene == null) {
-                    EditorSceneManager.playModeStartScene = null;
-                    UnityEngine.Debug.LogWarning($"SetPlayModeStartScene: Main scene not found at: {MainScenePath}");
-                    return;
-                }
-
+        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths, bool didDomainReload) {
+            SceneAsset scene = AssetDatabase.LoadAssetAtPath<SceneAsset>(MainScenePath);
+            if (scene == null) {
+                Debug.LogError($"SetPlayModeStartScene: Did not find main scene at '{MainScenePath}'. Using default scene-loading behavior instead.");
+                EditorSceneManager.playModeStartScene = null;
+            } else {
                 EditorSceneManager.playModeStartScene = scene;
             }
         }
