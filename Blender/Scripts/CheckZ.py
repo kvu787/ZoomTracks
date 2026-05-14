9db1fb861e90898f543e04ba23647ed8139ca463
Kevin Vu <kevin.m.vu@gmail.com>
2026-05-13 18:32:35 -0700

    Refactor and add CheckBarriers

--- Blender/Scripts/CheckZ.py
+++ Blender/Scripts/CheckZ.py
@@ -10,7 +10,7 @@ ValidZHeights = [
     0.09375,
 ]
 
-if __name__ == "__main__":
+def CheckZ():
     print("BEGIN -----------------------------------")
     objects = [obj for obj in bpy.context.selected_objects if obj.type == "MESH"]
     objects.sort(key=lambda obj: obj.name)
@@ -20,12 +20,44 @@ if __name__ == "__main__":
                 print(f"Name: {obj.name}")
                 print("Origin ERROR")
                 print(f"Origin: z={repr(obj.location.z)}")
+                print()
             for vertex in obj.data.vertices:
                 if vertex.co.z != 0.0:
                     print(f"Name: {obj.name}")
                     print("Vertex ERROR")
                     print(f"Vertex {vertex.index}: x={repr(vertex.co.x)}, y={repr(vertex.co.y)}, z={repr(vertex.co.z)}")
+                    print()
         print(f"{len(objects)} objects checked")
     else:
         print("No mesh objects found in selection")
     print("END -------------------------------------")
+
+def CheckBarriers():
+    print("BEGIN -----------------------------------")
+    objects = [obj for obj in bpy.context.selected_objects if obj.type == "MESH"]
+    objects.sort(key=lambda obj: obj.name)
+    if len(objects) > 0:
+        for obj in objects:
+            if obj.location.z not in ValidZHeights:
+                print(f"Name: {obj.name}")
+                print("Origin ERROR")
+                print(f"Origin: z={repr(obj.location.z)}")
+                print()
+            for vertex in obj.data.vertices:
+                if not (vertex.co.z == 0 and vertex.co.y == 0):
+                    print(f"Name: {obj.name}")
+                    print("Vertex ERROR")
+                    print(f"Vertex {vertex.index}: x={repr(vertex.co.x)}, y={repr(vertex.co.y)}, z={repr(vertex.co.z)}")
+                    print()
+        print(f"{len(objects)} objects checked")
+    else:
+        print("No mesh objects found in selection")
+    print("END -------------------------------------")
+
+
+def main():
+    CheckBarriers()
+
+if __name__ == "__main__":
+    main()
+
