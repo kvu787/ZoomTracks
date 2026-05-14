c1af62050eb5a7249639473327308b5e344fc634
Kevin Vu <kevin.m.vu@gmail.com>
2026-05-13 18:49:44 -0700

    Add "IgnoreRotation" variant to checker script

--- Blender/Scripts/CheckZ.py
+++ Blender/Scripts/CheckZ.py
@@ -54,9 +54,30 @@ def CheckBarriers():
         print("No mesh objects found in selection")
     print("END -------------------------------------")
 
+def CheckBarriers_IgnoreRotation():
+    print("BEGIN -----------------------------------")
+    objects = [obj for obj in bpy.context.selected_objects if obj.type == "MESH"]
+    objects.sort(key=lambda obj: obj.name)
+    if len(objects) > 0:
+        for obj in objects:
+            if obj.location.z not in ValidZHeights:
+                print(f"Name: {obj.name}")
+                print("Origin ERROR")
+                print(f"Origin: z={repr(obj.location.z)}")
+                print()
+            for vertex in obj.data.vertices:
+                if not (vertex.co.z == 0):
+                    print(f"Name: {obj.name}")
+                    print("Vertex ERROR")
+                    print(f"Vertex {vertex.index}: x={repr(vertex.co.x)}, y={repr(vertex.co.y)}, z={repr(vertex.co.z)}")
+                    print()
+        print(f"{len(objects)} objects checked")
+    else:
+        print("No mesh objects found in selection")
+    print("END -------------------------------------")
 
 def main():
-    CheckBarriers()
+    CheckBarriers_IgnoreRotation()
 
 if __name__ == "__main__":
     main()
