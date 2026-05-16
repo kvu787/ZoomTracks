0217a31d7652cfb288c66cc42009605028781816
Kevin Vu <kevin.m.vu@gmail.com>
2026-05-16 11:29:29 -0700

    Add logging method to investigate gamepad stick inputs

--- ZoomTracks/Assets/Scripts/GameDataAndLogic/InputManager.cs
+++ ZoomTracks/Assets/Scripts/GameDataAndLogic/InputManager.cs
@@ -1,4 +1,7 @@
+using System;
+using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 namespace ZoomTracks {
     public class InputManager {
@@ -8,6 +11,25 @@ namespace ZoomTracks {
         public void UpdateInputs() {
             this.Keyboard = Keyboard.current;
             this.Gamepad = Gamepad.current;
+            //this.LogGamepadRightStick();
+        }
+
+        private DateTime LastLogTime = DateTime.MinValue;
+        private const double TimeoutSeconds = 0.5;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0051:Remove unused private members", Justification = "<Pending>")]
+        private void LogGamepadRightStick() {
+            if (this.Gamepad != null) {
+                if ((DateTime.Now - this.LastLogTime) > TimeSpan.FromSeconds(TimeoutSeconds)) {
+                    this.LastLogTime = DateTime.Now;
+                    StickControl stickControl = this.Gamepad.rightStick;
+                    Vector2 processedValue = stickControl.ReadValue();
+                    Vector2 rawValue = stickControl.ReadUnprocessedValue();
+                    Debug.Log($"P: magnitude={processedValue.magnitude.ToExactDecimalString()}, x={processedValue.x.ToExactDecimalString()}, y={processedValue.y.ToExactDecimalString()}");
+                    Debug.Log($"R: magnitude={rawValue.magnitude.ToExactDecimalString()}, x={rawValue.x.ToExactDecimalString()}, y={rawValue.y.ToExactDecimalString()}");
+                    Debug.Log("==================================");
+                }
+            }
         }
     }
 }
