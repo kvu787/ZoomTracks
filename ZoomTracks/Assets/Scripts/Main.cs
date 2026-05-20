0fca1d1283a4214a0c0f7b5cf7cb866c2fed0218
Kevin Vu <kevin.m.vu@gmail.com>
2026-05-20 06:00:01 -0700

    Change starting track to Track1 and adjust track settings

--- ZoomTracks/Assets/Scripts/Main.cs
+++ ZoomTracks/Assets/Scripts/Main.cs
@@ -8,7 +8,7 @@ using UnityEngine.Scripting;
 namespace ZoomTracks {
     public class Main : MonoBehaviour {
         private const string UiSceneName = "Ui";
-        private const int InitialTrackSceneIndex = 1;
+        private const int InitialTrackSceneIndex = 0;
         private static IReadOnlyList<string> TrackSceneNames { get; } = Array.AsReadOnly(new[] {
             "Track1",
             "Track2",
