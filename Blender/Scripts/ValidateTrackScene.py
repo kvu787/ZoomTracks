99f7f40e05ceb5b932d35dbd2886da0021d6ac99
Kevin Vu <kevin.m.vu@gmail.com>
2026-05-24 06:14:41 -0700

    Add validation for view layer exclusions

--- Blender/Scripts/ValidateTrackScene.py
+++ Blender/Scripts/ValidateTrackScene.py
@@ -12,8 +12,22 @@ AllowedCollectionNames = [
     "Vehicles",
 ]
 
+def FindLayerCollection(layerCollection: bpy.types.LayerCollection, collection: bpy.types.Collection) -> bpy.types.LayerCollection | None:
+    if layerCollection.collection == collection:
+        return layerCollection
+    for childLayerCollection in layerCollection.children:
+        found = FindLayerCollection(childLayerCollection, collection)
+        if found is not None:
+            return found
+    return None
+
 def Main():
-    assert len(bpy.data.scenes) == 1
+    assert len(bpy.data.scenes) == 1, "Blender file must have exactly one scene"
+    scene = bpy.data.scenes[0]
+
+    assert len(scene.view_layers) == 1, "Scene must contain exactly one view layer"
+    viewLayer = scene.view_layers[0]
+    assert viewLayer.name == "ViewLayer"
 
     sceneCollection = bpy.data.scenes[0].collection
     assert len(sceneCollection.children) == 1, "Scene collection must contain exactly one collection"
@@ -28,6 +42,13 @@ def Main():
     for collectionName in collectionNames:
         if collectionName not in AllowedCollectionNames:
             raise Exception(f"Disallowed collection name: {collectionName}")
+
+    assert not FindLayerCollection(viewLayer.layer_collection, rootCollection).exclude
+    for collection in rootCollection.children:
+        if collection.name == "Camera" or collection.name == "Templates":
+            assert FindLayerCollection(viewLayer.layer_collection, collection).exclude
+        else:
+            assert not FindLayerCollection(viewLayer.layer_collection, collection).exclude
 
 if __name__ == "__main__":
     Main()
