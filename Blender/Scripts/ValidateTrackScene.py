0c4274af1c4656cf79a7b63f6ba19bdbe29aeda4
Kevin Vu <kevin.m.vu@gmail.com>
2026-05-24 05:18:34 -0700

    Improve track scene validation

--- Blender/Scripts/ValidateTrackScene.py
+++ Blender/Scripts/ValidateTrackScene.py
@@ -12,16 +12,22 @@ AllowedCollectionNames = [
     "Vehicles",
 ]
 
-def main():
+def Main():
     assert len(bpy.data.scenes) == 1
-    scene = bpy.data.scenes[0]
-    assert len(scene.collection.children) == 1
-    assert scene.collection.children[0].name == "Collection"
-    collectionNames = [c.name for c in scene.collection.children[0].children]
-    print(collectionNames)
+
+    sceneCollection = bpy.data.scenes[0].collection
+    assert len(sceneCollection.children) == 1, "Scene collection must contain exactly one collection"
+    assert len(sceneCollection.objects) == 0, "Scene collection cannot contain objects"
+
+    rootCollection = sceneCollection.children[0]
+    assert rootCollection.name == "Collection"
+    assert len(rootCollection.children) == len(AllowedCollectionNames), f"Root collection must contain exactly {len(AllowedCollectionNames)} collection"
+    assert len(rootCollection.objects) == 0, "Root collection cannot contain objects"
+
+    collectionNames = [c.name for c in rootCollection.children]
     for collectionName in collectionNames:
         if collectionName not in AllowedCollectionNames:
-            raise Exception(f"disallowed collection name: {collectionName}")
+            raise Exception(f"Disallowed collection name: {collectionName}")
 
 if __name__ == "__main__":
-    main()
+    Main()
