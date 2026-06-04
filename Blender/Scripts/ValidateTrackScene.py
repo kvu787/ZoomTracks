ac3f1fa4510fa1c004bbf8b079f3c63dfa654da6
Kevin Vu <kevin.m.vu@gmail.com>
2026-06-03 17:22:18 -0700

    Add validation for modifiers, non-uniform scales, and subd levels

--- Blender/Scripts/ValidateTrackScene.py
+++ Blender/Scripts/ValidateTrackScene.py
@@ -67,37 +67,63 @@ def FindLayerCollection(layerCollection: bpy.types.LayerCollection, collection: 
 def Main():
     print(f"{Path(__file__).name} started at {datetime.now()}")
 
+    # Verify scene count
     assert len(bpy.data.scenes) == 1, "Blender file must have exactly one scene"
     scene = bpy.data.scenes[0]
 
+    # Verify view layer
     assert len(scene.view_layers) == 1, "Scene must contain exactly one view layer"
     viewLayer = scene.view_layers[0]
     assert viewLayer.name == "ViewLayer"
 
+    # Verify scene collection properties
     sceneCollection = bpy.data.scenes[0].collection
     assert len(sceneCollection.children) == 1, "Scene collection must contain exactly one collection"
     assert len(sceneCollection.objects) == 0, "Scene collection cannot contain objects"
 
+    # Verify root collection properties
     rootCollection = sceneCollection.children[0]
     assert rootCollection.name == "Collection"
     assert len(rootCollection.children) == len(AllowedCollectionNames), f"Root collection must contain exactly {len(AllowedCollectionNames)} collection"
     assert len(rootCollection.objects) == 0, "Root collection cannot contain objects"
 
+    # Check for unknown collection names
     collectionNames = [c.name for c in rootCollection.children]
     for collectionName in collectionNames:
         if collectionName not in AllowedCollectionNames:
             raise Exception(f"Disallowed collection name: {collectionName}")
 
+    # Ensure collections are unhidden/hidden
     assert not FindLayerCollection(viewLayer.layer_collection, rootCollection).exclude, "Root collection is hidden"
     for collection in rootCollection.children:
-        if collection.name == "Camera" or collection.name == "Templates":
+        if collection.name in ("Camera", "Templates", "Uncategorized"):
             assert FindLayerCollection(viewLayer.layer_collection, collection).exclude, "Camera or Template collection is not hidden"
         else:
             assert not FindLayerCollection(viewLayer.layer_collection, collection).exclude, "This collection should not be hidden"
 
-    CheckOriginPositionAndRotation(bpy.data.objects)
+    # Check for modifiers that should be applied
+    for obj in bpy.data.objects:
+        for modifier in obj.modifiers:
+            if modifier.type == 'SUBSURF':
+                # print(f"{modifier.name}, {modifier.type}, {type(modifier)}")
+                pass
+            elif modifier.type == 'NODES':
+                # print(f"{modifier.name}, {modifier.type}, {type(modifier)}, {modifier.node_group.name}")
+                pass
+            else:
+                print(f"Warning: Unapplied modifier of type '{modifier.type}' on object '{obj.name}'")
 
-    # TODO: Warn on any unapplied modifiers that aren't subd or custom generators
+    # Check for non-uniform scales
+    # for obj in bpy.data.objects:
+    #     if not (obj.scale.x == obj.scale.y and obj.scale.y == obj.scale.z):
+    #         print(f"Non-uniform scale: {obj.name}, ({repr(obj.scale.x)}, {repr(obj.scale.y)}, {repr(obj.scale.z)})")
+
+    # Print out subd levels
+    for obj in bpy.data.objects:
+        for modifier in obj.modifiers:
+            if modifier.type == 'SUBSURF':
+                print(f"Subd modifier: {obj.name}, levels={modifier.levels}")
+
     print(f"{Path(__file__).name} finished at {datetime.now()}")
 
 if __name__ == "__main__":
