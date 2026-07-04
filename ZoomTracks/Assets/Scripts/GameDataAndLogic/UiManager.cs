e91fdc8835ef591a30939cd2dcbb26f59c016ebb
Kevin Vu <kevin.m.vu@gmail.com>
2026-07-04 07:15:59 -0700

    Add display mode to UI, right-justify camera size and display mode, fix sizing

--- ZoomTracks/Assets/Scripts/GameDataAndLogic/UiManager.cs
+++ ZoomTracks/Assets/Scripts/GameDataAndLogic/UiManager.cs
@@ -6,18 +6,27 @@ namespace ZoomTracks {
     public class UiManager {
         private TMP_Text ClockText { get; }
         private TMP_Text CameraSizeText { get; }
+        private TMP_Text DisplayModeText { get; }
         private CameraController CameraController { get; }
 
         public UiManager(CameraController cameraController) {
             this.ClockText = GameObject.Find(nameof(this.ClockText)).GetComponent<TMP_Text>();
             this.CameraSizeText = GameObject.Find(nameof(this.CameraSizeText)).GetComponent<TMP_Text>();
+            this.DisplayModeText = GameObject.Find(nameof(this.DisplayModeText)).GetComponent<TMP_Text>();
             this.CameraController = cameraController;
         }
 
         public void UpdateUi() {
             //this.ClockText.SetText(DateTimeUtility.GetDateTimeNowChars_Buffered());
             this.ClockText.text = $"{DateTime.Now:yyyy-MM-dd [tt] HH:mm:ss.fff}";
-            this.CameraSizeText.text = $"Camera size: {this.CameraController.OrthographicCameraSize}";
+            this.CameraSizeText.text = $"Camera size: {this.CameraController.OrthographicCameraSize:F2}";
+            this.DisplayModeText.text = Screen.fullScreenMode switch {
+                FullScreenMode.ExclusiveFullScreen => "Display mode: Exclusive",
+                FullScreenMode.FullScreenWindow => "Display mode: Borderless",
+                FullScreenMode.Windowed => "Display mode: Windowed",
+                FullScreenMode.MaximizedWindow => "Display mode: Windowed max",
+                _ => throw new Exception($"Unrecognized FullScreenMode: {Screen.fullScreenMode}"),
+            };
         }
     }
 }
