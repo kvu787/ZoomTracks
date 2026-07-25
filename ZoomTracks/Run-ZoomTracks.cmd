887be521a5c508e9563e26476be27e7e8842f72c
Kevin Vu <kevin.m.vu@gmail.com>
2026-07-25 08:55:55 -0700

    Fix paths

--- ZoomTracks/Run-ZoomTracks.cmd
+++ ZoomTracks/Run-ZoomTracks.cmd
@@ -1,1 +1,1 @@
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\Users\kevin\Repository\Unity\ZoomTracks\ZoomTracks\Run-ZoomTracks.ps1"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\Run-ZoomTracks.ps1"
--- ZoomTracks/Run-ZoomTracks.ps1
+++ ZoomTracks/Run-ZoomTracks.ps1
@@ -1,15 +1,15 @@
 Set-StrictMode -Version "Latest"
 $ErrorActionPreference = "Stop"
 
-$logFolderPath = "C:\Users\kevin\Repository\Unity\ZoomTracks\ZoomTracks\MyLogOutput\$(Get-Date -Format "yyyy-MM-dd_HH-mm-ss")"
+$logFolderPath = "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\MyLogOutput\$(Get-Date -Format "yyyy-MM-dd_HH-mm-ss")"
 New-Item -ItemType "Directory" -Path $logFolderPath
 
-# PresentMon
-$presentMonLogFilePath  = "$($logFolderPath)\PresentMon.csv"
-Start-Process `
-  -FilePath "C:\Users\kevin\Program\PresentMon-2.5.1-x64.exe" `
-  -ArgumentList "--process_name `"ZoomTracks.exe`" --output_file `"$($presentMonLogFilePath)`"" `
-  -Verb "RunAs"
+# # PresentMon
+# $presentMonLogFilePath  = "$($logFolderPath)\PresentMon.csv"
+# Start-Process `
+#   -FilePath "C:\Users\k\Program\PresentMon-2.5.1-x64.exe" `
+#   -ArgumentList "--process_name `"ZoomTracks.exe`" --output_file `"$($presentMonLogFilePath)`"" `
+#   -Verb "RunAs"
 
 $registryPath = "HKCU:\Software\K\ZoomTracks"
 if (Test-Path $registryPath) {
@@ -25,14 +25,14 @@ $stutterLogFilePath = "$($logFolderPath)\Stutter.log"
 # # Monitor 1, dx12 1440p, borderless fullscreen
 # $process = `
 #     Start-Process `
-#         -FilePath "C:\Users\kevin\Repository\Unity\ZoomTracks\ZoomTracks\MyBuildOutput\ZoomTracks.exe" `
+#         -FilePath "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\MyBuildOutput\ZoomTracks.exe" `
 #         -ArgumentList "-monitor 1 -force-d3d12 -force-gfx-direct -window-mode borderless -screen-width 2560 -screen-height 1440 -logFile `"$($unityLogFilePath)`" -timestamps -refreshRate 240 -stutterLogFilePath `"$($stutterLogFilePath)`"" `
 #         -PassThru
 
 # Monitor 1, dx11, 1440p, borderless fullscreen
 $process = `
     Start-Process `
-        -FilePath "C:\Users\kevin\Repository\Unity\ZoomTracks\ZoomTracks\MyBuildOutput\ZoomTracks.exe" `
+        -FilePath "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\MyBuildOutput\ZoomTracks.exe" `
         -ArgumentList "-monitor 1 -force-d3d11 -force-gfx-direct  -window-mode borderless -force-d3d11-flip-model -screen-width 2560 -screen-height 1440 -logFile `"$($unityLogFilePath)`" -timestamps -refreshRate -1 -stutterLogFilePath `"$($stutterLogFilePath)`"" `
         -PassThru
 
