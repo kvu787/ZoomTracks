aa90b2848caa351cc9585f4564b9d1d14165912f
Kevin Vu <kevin.m.vu@gmail.com>
2026-07-28 04:57:14 -0700

    Update PowerShell launcher

--- ZoomTracks/Run-ZoomTracks.ps1
+++ ZoomTracks/Run-ZoomTracks.ps1
@@ -4,12 +4,12 @@ $ErrorActionPreference = "Stop"
 $logFolderPath = "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\MyLogOutput\$(Get-Date -Format "yyyy-MM-dd_HH-mm-ss")"
 New-Item -ItemType "Directory" -Path $logFolderPath
 
-# # PresentMon
-# $presentMonLogFilePath  = "$($logFolderPath)\PresentMon.csv"
-# Start-Process `
-#   -FilePath "C:\Users\k\Program\PresentMon-2.5.1-x64.exe" `
-#   -ArgumentList "--process_name `"ZoomTracks.exe`" --output_file `"$($presentMonLogFilePath)`"" `
-#   -Verb "RunAs"
+# PresentMon
+$presentMonLogFilePath  = "$($logFolderPath)\PresentMon.csv"
+Start-Process `
+  -FilePath "C:\Users\k\Program\PresentMon-2.5.1-x64.exe" `
+  -ArgumentList "--process_name `"ZoomTracks.exe`" --output_file `"$($presentMonLogFilePath)`"" `
+  -Verb "RunAs"
 
 $registryPath = "HKCU:\Software\K\ZoomTracks"
 if (Test-Path $registryPath) {
@@ -19,21 +19,13 @@ if (Test-Path $registryPath) {
 $unityLogFilePath = "$($logFolderPath)\Unity.log"
 $stutterLogFilePath = "$($logFolderPath)\Stutter.log"
 
-# Use exact frequency as reported by "Settings > System > Display > Advanced display"
-# $refreshRate = 179.84
+# When specifying refresh rate, use exact frequency as reported by "Settings > System > Display > Advanced display".
 
-# # Monitor 1, dx12 1440p, borderless fullscreen
-# $process = `
-#     Start-Process `
-#         -FilePath "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\MyBuildOutput\ZoomTracks.exe" `
-#         -ArgumentList "-monitor 1 -force-d3d12 -force-gfx-direct -window-mode borderless -screen-width 2560 -screen-height 1440 -logFile `"$($unityLogFilePath)`" -timestamps -refreshRate 240 -stutterLogFilePath `"$($stutterLogFilePath)`"" `
-#         -PassThru
-
-# Monitor 1, dx11, 1440p, borderless fullscreen
+# Monitor 1, dx11, 3840*2400, borderless fullscreen
 $process = `
     Start-Process `
         -FilePath "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\MyBuildOutput\ZoomTracks.exe" `
-        -ArgumentList "-monitor 1 -force-d3d11 -force-gfx-direct  -window-mode borderless -force-d3d11-flip-model -screen-width 2560 -screen-height 1440 -logFile `"$($unityLogFilePath)`" -timestamps -refreshRate -1 -stutterLogFilePath `"$($stutterLogFilePath)`"" `
+        -ArgumentList "-monitor 1 -force-d3d11 -force-gfx-direct  -window-mode borderless -force-d3d11-flip-model -screen-width 3840 -screen-height 2400 -logFile `"$($unityLogFilePath)`" -timestamps -refreshRate -1 -stutterLogFilePath `"$($stutterLogFilePath)`"" `
         -PassThru
 
 $process.PriorityClass = [System.Diagnostics.ProcessPriorityClass]::High
