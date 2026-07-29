c26e98f9554441ff342fa3be340b1392bbffc458
Kevin Vu <kevin.m.vu@gmail.com>
2026-07-29 14:07:21 -0700

    Update run  configs

--- ZoomTracks/Run-ZoomTracks.ps1
+++ ZoomTracks/Run-ZoomTracks.ps1
@@ -21,12 +21,26 @@ $stutterLogFilePath = "$($logFolderPath)\Stutter.log"
 
 # When specifying refresh rate, use exact frequency as reported by "Settings > System > Display > Advanced display".
 
-# Monitor 1, dx11, 3840*2400, borderless fullscreen
+# # Monitor 1, dx11, 2560*1440, exclusive fullscreen (fullscreen optimizations must be disabled in process properties)
+# $process = `
+#     Start-Process `
+#         -FilePath "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\MyBuildOutput\ZoomTracks.exe" `
+#         -ArgumentList "-monitor 1 -force-d3d11 -force-gfx-direct -window-mode exclusive -screen-width 2560 -screen-height 1440 -logFile `"$($unityLogFilePath)`" -timestamps -refreshRate -1 -stutterLogFilePath `"$($stutterLogFilePath)`"" `
+#         -PassThru
+
+# Monitor 1, dx11, 2560*1440, borderless fullscreen
 $process = `
     Start-Process `
         -FilePath "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\MyBuildOutput\ZoomTracks.exe" `
-        -ArgumentList "-monitor 1 -force-d3d11 -force-gfx-direct  -window-mode borderless -force-d3d11-flip-model -screen-width 3840 -screen-height 2400 -logFile `"$($unityLogFilePath)`" -timestamps -refreshRate -1 -stutterLogFilePath `"$($stutterLogFilePath)`"" `
+        -ArgumentList "-monitor 1 -force-d3d11 -force-gfx-direct -window-mode borderless -force-d3d11-flip-model -screen-width 2560 -screen-height 1440 -logFile `"$($unityLogFilePath)`" -timestamps -refreshRate -1 -stutterLogFilePath `"$($stutterLogFilePath)`"" `
         -PassThru
+
+# # Monitor 1, dx12, 2560*1440, borderless fullscreen
+# $process = `
+#     Start-Process `
+#         -FilePath "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\MyBuildOutput\ZoomTracks.exe" `
+#         -ArgumentList "-monitor 1 -force-d3d12 -force-gfx-direct -window-mode borderless -screen-width 2560 -screen-height 1440 -logFile `"$($unityLogFilePath)`" -timestamps -refreshRate -1 -stutterLogFilePath `"$($stutterLogFilePath)`"" `
+#         -PassThru
 
 $process.PriorityClass = [System.Diagnostics.ProcessPriorityClass]::High
 Write-Host "Launched with PID=$($process.Id)"
