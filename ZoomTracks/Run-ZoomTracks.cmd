f092d03dbb88bd73c12726b7643c306a743b89b9
Kevin Vu <kevin.m.vu@gmail.com>
2026-08-02 03:58:53 -0700

    Rename launcher scripts

--- ZoomTracks/Run-ZoomTracks.cmd
+++
@@ -1,1 +0,0 @@
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\Run-ZoomTracks.ps1"
---
+++ ZoomTracks/Run.cmd
@@ -0,0 +1,1 @@
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\Run.ps1"
