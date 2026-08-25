a9735b4d0dbd6f131d624914cda7ab79f3fd9d23
Kevin Vu <kevin.m.vu@gmail.com>
2026-08-25 15:34:14 -0700

    Run standalone build with dx12 by default. Unity Editor remains dx11.

--- ZoomTracks/Run.ps1
+++ ZoomTracks/Run.ps1
@@ -23,19 +23,19 @@ $stutterLogFilePath = "$($logFolderPath)\Stutter.log"
 
 # When specifying refresh rate, use exact frequency as reported by "Settings > System > Display > Advanced display".
 
-# # Monitor 1, dx12, 2560*1440, borderless fullscreen
-# $process = `
-#     Start-Process `
-#         -FilePath "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\MyBuildOutput\ZoomTracks.exe" `
-#         -ArgumentList "-monitor 1 -force-d3d12 -window-mode borderless -screen-width 2560 -screen-height 1440 -logFile `"$($unityLogFilePath)`" -timestamps -refreshRate -1 -stutterLogFilePath `"$($stutterLogFilePath)`"" `
-#         -PassThru
-
-# Monitor 1, dx11, 2560*1440, borderless fullscreen
+# Monitor 1, dx12, 2560*1440, borderless fullscreen
 $process = `
     Start-Process `
         -FilePath "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\MyBuildOutput\ZoomTracks.exe" `
-        -ArgumentList "-monitor 1 -force-d3d11 -window-mode borderless -force-d3d11-flip-model -screen-width 2560 -screen-height 1440 -logFile `"$($unityLogFilePath)`" -timestamps -refreshRate -1 -stutterLogFilePath `"$($stutterLogFilePath)`"" `
+        -ArgumentList "-monitor 1 -force-d3d12 -window-mode borderless -screen-width 2560 -screen-height 1440 -logFile `"$($unityLogFilePath)`" -timestamps -refreshRate -1 -stutterLogFilePath `"$($stutterLogFilePath)`"" `
         -PassThru
+
+# # Monitor 1, dx11, 2560*1440, borderless fullscreen
+# $process = `
+#     Start-Process `
+#         -FilePath "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\MyBuildOutput\ZoomTracks.exe" `
+#         -ArgumentList "-monitor 1 -force-d3d11 -window-mode borderless -force-d3d11-flip-model -screen-width 2560 -screen-height 1440 -logFile `"$($unityLogFilePath)`" -timestamps -refreshRate -1 -stutterLogFilePath `"$($stutterLogFilePath)`"" `
+#         -PassThru
 
 $process.PriorityClass = [System.Diagnostics.ProcessPriorityClass]::High
 Write-Host "Launched with PID=$($process.Id)"
