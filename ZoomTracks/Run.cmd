17d6a6a83b5bdb4dbfa20a42fe74ab625679ee08
Kevin Vu <kevin.m.vu@gmail.com>
2026-09-15 16:30:29 -0700

    Fix paths

--- ZoomTracks/Run.cmd
+++ ZoomTracks/Run.cmd
@@ -1,1 +1,1 @@
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\Run.ps1"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\Users\k\Repository\ZoomTracks\ZoomTracks\Run.ps1"
--- ZoomTracks/Run.ps1
+++ ZoomTracks/Run.ps1
@@ -1,7 +1,7 @@
 Set-StrictMode -Version "Latest"
 $ErrorActionPreference = "Stop"
 
-$logFolderPath = "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\MyLogOutput\$(Get-Date -Format "yyyy-MM-dd_HH-mm-ss")"
+$logFolderPath = "C:\Users\k\Repository\ZoomTracks\ZoomTracks\MyLogOutput\$(Get-Date -Format "yyyy-MM-dd_HH-mm-ss")"
 New-Item -ItemType "Directory" -Path $logFolderPath
 
 # $presentMonPath = "$env:UserProfile\Program\PresentMon-2.5.1-x64.exe"
@@ -26,14 +26,14 @@ $stutterLogFilePath = "$($logFolderPath)\Stutter.log"
 # Monitor 1, dx12, 2560*1440, borderless fullscreen
 $process = `
     Start-Process `
-        -FilePath "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\MyBuildOutput\ZoomTracks.exe" `
+        -FilePath "C:\Users\k\Repository\ZoomTracks\ZoomTracks\MyBuildOutput\ZoomTracks.exe" `
         -ArgumentList "-monitor 1 -force-d3d12 -window-mode borderless -screen-width 2560 -screen-height 1440 -logFile `"$($unityLogFilePath)`" -timestamps -refreshRate -1 -stutterLogFilePath `"$($stutterLogFilePath)`"" `
         -PassThru
 
 # # Monitor 1, dx11, 2560*1440, borderless fullscreen
 # $process = `
 #     Start-Process `
-#         -FilePath "C:\Users\k\Repository\Unity\ZoomTracks\ZoomTracks\MyBuildOutput\ZoomTracks.exe" `
+#         -FilePath "C:\Users\k\Repository\ZoomTracks\ZoomTracks\MyBuildOutput\ZoomTracks.exe" `
 #         -ArgumentList "-monitor 1 -force-d3d11 -window-mode borderless -force-d3d11-flip-model -screen-width 2560 -screen-height 1440 -logFile `"$($unityLogFilePath)`" -timestamps -refreshRate -1 -stutterLogFilePath `"$($stutterLogFilePath)`"" `
 #         -PassThru
 
