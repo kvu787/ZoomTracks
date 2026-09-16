b58ac18a28ee3f72712cdaf79d6d4ac9752e29d5
Kevin Vu <kevin.m.vu@gmail.com>
2026-09-15 19:56:27 -0700

    Create Track005, which loosely resembles a high-top shoe

--- ZoomTracks/Assets/Scripts/Main.cs
+++ ZoomTracks/Assets/Scripts/Main.cs
@@ -12,13 +12,14 @@ namespace ZoomTracks {
         private const string RefreshRateFlag = "-refreshRate";
         private const string StutterLogFilePathFlag = "-stutterLogFilePath";
         private const string UiSceneName = "Ui";
-        private const int InitialTrackIndex = 4;
+        private const int InitialTrackIndex = 5;
         private static IReadOnlyList<string> TrackNames { get; } = Array.AsReadOnly(new[] {
             "Basic",
             "Track001",
             "Track002",
             "Track003",
             "Track004",
+            "Track005",
         });
 
         private TimeSpan TimeoutDurationSeconds { get; } = TimeSpan.FromSeconds(0.35);
